45b2dfbbeda21605a764b6fdf47a608bca28bfab
Brandon King <brandon@kingscocoa.com>
2013-04-26 20:21:58 -0400

    Completed project

--- StarGif/StarGif/ViewController.m
+++ StarGif/StarGif/ViewController.m
@@ -9,21 +9,43 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property (nonatomic, weak) IBOutlet UIImageView *starImageView;
 @end
+
+
 
 @implementation ViewController
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	
+    NSMutableArray *starImageArray = [@[] mutableCopy];
+    for (int i = 0; i < 8; i++) {
+        UIImage *starImage = [UIImage imageNamed:[NSString stringWithFormat:@"star_%d", i]];
+        [starImageArray addObject:starImage];
+    }
+
+    self.starImageView.animationImages = starImageArray;
+    self.starImageView.animationRepeatCount = 1;
+    self.starImageView.animationDuration = 0.267;
+
+    self.starImageView.image = starImageArray[0];
 }
 
-- (void)didReceiveMemoryWarning
+- (IBAction)animateButtonTouched
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    if (!self.starImageView.isAnimating) {
+        [self.starImageView startAnimating];
+        [self.starImageView setImage:[self.starImageView.animationImages lastObject]];
+        [self performSelector:@selector(starAnimationDidFinish) withObject:nil afterDelay:self.starImageView.animationDuration];
+    }
+}
+
+- (void)starAnimationDidFinish
+{
+    NSArray *reversedImages = [[self.starImageView.animationImages reverseObjectEnumerator] allObjects];
+    self.starImageView.animationImages = reversedImages;
 }
 
 @end
